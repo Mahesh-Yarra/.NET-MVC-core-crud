e693165f029550935acd6f2de2336511cfaa7e40
Mahesh-Yarra <mahesh.kyarra@gmail.com>
2024-08-14 11:23:05 +0530

    Added the project

---
+++ corecrud/Controllers/StudentController.cs
@@ -0,0 +1,88 @@
+﻿using corecrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace corecrud.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext ctx;
+        public StudentController(StudentContext context)
+        {
+            this.ctx = context;
+        }
+        public IActionResult ShowAllStudents()
+        {
+            var students = ctx.Students.ToList();
+            return View(students);
+        }
+
+        public IActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent(Student model)
+        {
+            if(!ModelState.IsValid)
+                return View(model);
+            try
+            {
+                ctx.Students.Add(model);
+                await ctx.SaveChangesAsync();
+                ModelState.AddModelError("msg", "Data has saved successfully");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("msg", "Data could not saved");
+            }
+
+            return View();
+        }
+
+        public IActionResult EditStudent(int id)
+        {
+            var student = ctx.Students.Find(id);
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditStudent(Student model)
+        {
+            if(!ModelState.IsValid)
+                return View(model);
+
+            try
+            {
+                ctx.Entry(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                await ctx.SaveChangesAsync();
+                return RedirectToAction("ShowAllStudents");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("msg", "Error on updation");
+                return View();
+            }
+            
+        }
+
+        public IActionResult DeleteStudent(int id)
+        {
+            try
+            {
+                var student = ctx.Students.Find(id);
+                if (student!=null)
+                {
+                    ctx.Students.Remove(student);
+                    ctx.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+            return RedirectToAction("ShowAllStudents");
+        }
+    }
+    
+}
